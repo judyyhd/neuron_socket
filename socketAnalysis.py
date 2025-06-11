02f1c3290b4fc80403f6945e26f6dd8ec9ca39d8
Judy Yang <hy1331@nyu.edu>
2025-06-11 11:47:36 +0800

    parsed time

--- socketAnalysis.py
+++ socketAnalysis.py
@@ -9,4 +9,8 @@ import glob
 file_list = glob.glob('*.xlsx')
 dfs = [pd.read_excel(file) for file in file_list]
 df_concat = pd.concat(dfs, ignore_index=True)
+# %% Parse time
+df_concat['time'] = pd.to_datetime(df_concat['time'])
+df_concat['date'] = df_concat['time'].dt.date
+df_concat['hour'] = df_concat['time'].dt.hour
 # %%
